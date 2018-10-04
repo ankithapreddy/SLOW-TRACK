81bcb9cbb27344abecd61ebcf1932edf240ca3cd
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2018-10-04 17:50:33 +0530

    Update day4fourth.c

--- day4fourth.c
+++ day4fourth.c
@@ -1,30 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
-void main()
+int main()
 {
-    int sum(int);
+    int sum(int );
     int n,a;
     printf("Enter size of array: ");
     scanf("%d",&n);
     a=sum(n);
-    /*printf("Ans=%d",a);*/
+    printf("sum=%d",a);
+
+
 }
 int sum(int n)
 {
-    int i,s;
+    int s=0,i;
     int arr[100];
     for (i=0;i<n;i++)
     {
         printf("Enter the array element[%d]:",i+1);
         scanf("%d",&arr[i]);
     }
+
     for(i=0;i<n;i++)
     {
-        printf("%d",arr[i]);
-    }
-    /*for(i=0;i<n;i++)
-    {
         s=s+arr[i];
-    }*/
-    return 0;
+    }
+
+    return s;
 }
