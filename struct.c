340a4b8a539a5ac2d2b59cd8b94a80f6f28476f2
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2018-10-26 17:23:45 +0530

    Update struct.c

--- struct.c
+++ struct.c
@@ -3,7 +3,6 @@ struct fraction
 {
   int num;
   int den;
-  
 };
 
 struct fraction input()
@@ -11,15 +10,17 @@ struct fraction input()
 struct fraction f;
 printf("Enter the values of numerator and denominator:\n");
 scanf("%d%d",&f.num,&f.den);
+return f;
 }
-
 void output(struct fraction f)
 {
- printf("the fraction is :%d/%d",f.num,f.den);
+ printf("The fraction is:%d/%d",f.num,f.den);
 }
 int main()
 {
-input();
-void output(struct fraction f);
+struct fraction f;
+f=input();
+output(f);
 }
+
 
